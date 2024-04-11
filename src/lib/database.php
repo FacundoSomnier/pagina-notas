b65af381d98f9f23d64c98a2399204e48917025e
Facundo <facundosomnier@hotmail.com>
2024-04-11 10:09:19 -0300

    Initial commit Add getenv

--- src/lib/database.php
+++ src/lib/database.php
@@ -14,16 +14,17 @@ class Database {
 
 
     public function __construct(){
-        $this->host = 'localhost';
-        $this->db = 'notebook';
-        $this->user = 'root';
-        $this->password = '';
+        $this->host = getenv('DB_HOST');
+        $this->db = getenv('DB_NAME');
+        $this->user = getenv('DB_USER');
+        $this->password = getenv('DB_PASSWORD');
         $this->charset = 'utf8mb4';
+        $this->port = getenv('DB_PORT');
     }
 
     public function connect () {
         try {
-            $connection = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+            $connection = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}; port={$this->port}";
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES => false,
